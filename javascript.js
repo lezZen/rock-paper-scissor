7f86ba3e28b3a684442b6e8227c51b696edadef1
lezZen <yanezbruce123@gmail.com>
2022-09-08 10:23:25 -0500

    created playRound function

--- javascript.js
+++ javascript.js
@@ -2,15 +2,64 @@ function getComputerChoice(){
 let choice = Math.round(Math.random() * 2)
     switch(choice){
         case 0:
-            console.log('rock')
-            break;
+            return 'rock'
         case 1:
-            console.log('paper')
-            break;
+            return 'paper'
+            
         case 2:
-            console.log('scissor')        
-            break;
+            return 'scissors'                 
     }
 }
-getComputerChoice();
+// another version of playRound
+/*function playRound(playerSelection, computerSelection){
 
+    if((playerSelection == 'rock') && computerSelection == 'paper'){
+        return 'you lose, paper beats rock'
+    }else if ((playerSelection == 'rock') && computerSelection == 'scissors'){
+        return 'you win, rock beats scissors'
+    }
+    else if((playerSelection == "paper") && computerSelection == 'scissors'){
+        return 'you lose, scissors beats paper'
+    }else if((playerSelection == "paper") && computerSelection == 'rock'){
+        return 'you win, paper beats rock'
+    }
+    else if((playerSelection == "scissors") && computerSelection =='rock'){
+        return 'you lose, rock beats scissors'
+    }else if((playerSelection == "scissors") && computerSelection =='paper'){
+        return 'you win, scissors beats paper'
+    }else{
+        return 'draw'
+    }
+}*/
+function playRound(playerSelection, computerSelection){
+switch(playerSelection){
+    case "rock":
+        if(computerSelection == 'paper'){
+            return 'you lose, paper beats rock'
+        }else if(computerSelection == 'scissors'){
+            return "you win, rock beats scissors"
+        }else{
+            return 'draw'
+        }
+        
+    case 'scissors':
+        if(computerSelection == 'rock'){
+            return 'you lose, rock beats scissors'
+        }else if(computerSelection == 'paper'){
+            return "you win, scissors beats paper "
+        }else{
+            return 'draw'
+        }
+    case 'paper':
+        if(computerSelection == 'scissors'){
+            return 'you lose, scissors beats paper'
+        }else if(computerSelection == 'rock'){
+            return "you win, paper beats rock"
+        }else{
+            return 'draw'
+        }
+}
+}
+const computerSelection = getComputerChoice();
+const playerSelection= (prompt('select rock, paper or scissors')).toLowerCase()
+console.log(playRound(playerSelection, computerSelection))
